d0a26d3ea3dd539ba33eea1caba7d702a92d9e1c
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2022-03-09 16:05:11 +0800

    A*search

--- src/AStarPathFinding.py
+++ src/AStarPathFinding.py
@@ -29,33 +29,37 @@ class AStarPathFinding:
         # Set up a PriorityQueue to be used as our frontier of nodes to be explored
         Frontier = PriorityQueue()
 
-        # Initialize the frontier by inserting the start node
-        Frontier.put([0, StartNode])
+        # Initialize the frontier by inserting the start node with 0 energy cost, 0 distance and 0 priority(fCost from g + h)
+        #each element is in the order of (priority,distance,(node, cost))
+        Frontier.put((0, 0, (StartNode, 0)))
 
         # Initiallize other required parameters
         Explored = {}  # Dict of explored nodes {node : parentNode}
         DistCost = {}  # Dict of dist cost from start to end node {node : cost}
-        FCost = {}  # Dict of dict cost + heuristic cost from start to end node {node : cost}
-        EnergyCost = {}  # Dict of energy cost from start to end node {node : cost}
+        minCost = {}  # Dict to store the lowest cost for the specific node
+        minDist = {}  # Dict to store the lowest dist for the specific node
 
         # Initiallize values for starting node
-        Explored[StartNode] = None  # Start node no parent
-        DistCost[StartNode] = 0  # Start to Start cost = 0
-        FCost[StartNode] = 0
-        EnergyCost[StartNode] = 0  # Start to Start cost = 0
+        Explored[(StartNode, 0)] = None  # Start node no parent
+
+        DistCost[(StartNode, 0)] = 0  # Start to Start cost = 0
 
         # Repeat the process until either the frontier is empty or the goal has been reached
         while not Frontier.empty():
-            current = Frontier.get()[
-                1]  # Get the first vertex in the frontier to be explored ([1] is used to get the node id)
+            currentP, currentDist, (current, currentCost) = Frontier.get()  # Get the first vertex in the frontier to be explored ([1] is used to get the node id)
+
+            # If there exist a short path to current node/vertex we can go to check the next element in the priority queue
+            if current in minDist and current in minCost and minDist[current] <= currentDist and minCost[current] <= currentCost:
+                continue
+            # Update the min dictionary for dist and cost when there exist a better Distance or better Energy cost to the current node that we are visiting
+            if current not in minDist or minDist[current] > currentDist:
+                minDist[current] = currentDist
+            if current not in minCost or minCost[current] > currentCost:
+                minCost[current] = currentCost
+
             # Check if the current node is the goal
             if current == EndNode:
                 break
-
-            #if current in DistCost and current in EnergyCost and
-
-
-
             # If yet to reach goal, explore all neighbours adjacent to current node
             neighbours = G[current]
             for next in neighbours:  # Explore all adjacent nodes
@@ -63,35 +67,39 @@ class AStarPathFinding:
                 dist = Dist[current + "," + next]
                 energy = Cost[current + "," + next]
                 # Calculate total costs based on current node
-                newDist = DistCost[current] + dist
-                newEnergy = EnergyCost[current] + energy
+                newDist = DistCost[(current, currentCost)] + dist
+                newEnergy = currentCost + energy
 
                 # Consider whether to explore this node
                 # Either unexplored or the new costs to this node is better than the recorded ones
-                if (next not in Explored) or (newDist < DistCost[next]):
-                    # Check energy cost, if too high, don't explore
-                    if newEnergy <= EnergyCap:
-                        priority = newDist + (self.heuristic(current, next, Coord))
-                        # Insert the next node into the frontier, with its distance as the exploration prioity
-                        Frontier.put((priority, next))
-                        # Store its costs
-                        DistCost[next] = newDist
-                        EnergyCost[next] = newEnergy
-                        # Update the exploration status and set the current node to be the exploration parent node
-                        Explored[next] = current
+                # Check energy cost, if too high, don't explore
+                if (next, newEnergy) not in Explored and newEnergy <= EnergyCap:
+                    # calculate the A*'s FCost to be used as Priority
+                    priority = newDist + (self.heuristic(current, next, Coord))
+                    # Insert the next node into the frontier, with its fCost as the exploration priority
+                    Frontier.put((priority, newDist, (next, newEnergy)))
+                    # Store its costs
+                    DistCost[(next, newEnergy)] = newDist
 
+                    # Update the exploration status and set the current node to be the exploration parent node
+                    Explored[(next, newEnergy)] = (current, currentCost)
+
+        for itr in DistCost:
+            if itr[0] == '50':
+                print(DistCost[itr[0], minCost['50']])
+                print(minCost['50'])
         # Store calculated data into class
         self.ExploredSet = Explored
-        self.TotalDistance = DistCost[EndNode]
-        self.TotalEnergy = EnergyCost[EndNode]
-        self.setupExplorationResultString(EndNode)
+        self.TotalDistance = DistCost[EndNode,minCost[EndNode]]
+        self.TotalEnergy = minCost[EndNode]
+        self.setupExplorationResultString(EndNode,minCost[EndNode])
         return self
 
-    def setupExplorationResultString(self, EndNode):
+    def setupExplorationResultString(self, EndNode, MinCost):
         result = ""
-        current = self.ExploredSet[EndNode]
+        current = self.ExploredSet[EndNode, MinCost]
         while current != None:
-            result = current + "->" + result
+            result = current[0] + "->" + result
             current = self.ExploredSet[current]
         self.ShortestPath = result + EndNode
         return self
